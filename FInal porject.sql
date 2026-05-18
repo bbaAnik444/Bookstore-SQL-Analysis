ad4f9c53072d2782116f6eb335fbe2d0dd3d0bb9
bbaAnik444 <114015951+bbaAnik444@users.noreply.github.com>
2026-05-19 00:22:25 +0200

    Add files via upload
    bookstore SQL analysis project files

---
+++ FInal porject.sql
@@ -0,0 +1,171 @@
+-- Create Tables
+DROP TABLE IF EXISTS Books;
+CREATE TABLE Books (
+    Book_ID SERIAL PRIMARY KEY,
+    Title VARCHAR(100),
+    Author VARCHAR(100),
+    Genre VARCHAR(50),
+    Published_Year INT,
+    Price NUMERIC(10, 2),
+    Stock INT
+);
+DROP TABLE IF EXISTS customers;
+CREATE TABLE Customers (
+    Customer_ID SERIAL PRIMARY KEY,
+    Name VARCHAR(100),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    City VARCHAR(50),
+    Country VARCHAR(150)
+);
+DROP TABLE IF EXISTS orders;
+CREATE TABLE Orders (
+    Order_ID SERIAL PRIMARY KEY,
+    Customer_ID INT REFERENCES Customers(Customer_ID),
+    Book_ID INT REFERENCES Books(Book_ID),
+    Order_Date DATE,
+    Quantity INT,
+    Total_Amount NUMERIC(10, 2)
+);
+
+SELECT * FROM Books;
+SELECT * FROM Customers;
+SELECT * FROM Orders;
+
+
+-- Import Data into Books Table
+COPY Books(Book_ID, Title, Author, Genre, Published_Year, Price, Stock) 
+FROM 'D:\Study\SQL\Practice Files SQL Projects\Practice Files SQL Projects\Books.csv' 
+CSV HEADER;
+
+COPY Customers(Customer_ID, Name, Email, Phone, City, Country) 
+FROM 'D:\Study\SQL\Practice Files SQL Projects\Practice Files SQL Projects\Customers.csv' 
+CSV HEADER;
+
+COPY Orders(Order_ID, Customer_ID, Book_ID, Order_Date, Quantity, Total_Amount) 
+FROM 'D:\Study\SQL\Practice Files SQL Projects\Practice Files SQL Projects\Orders.csv' 
+CSV HEADER;
+
+
+--1) Retrieve all books in the "Fiction" genre
+select * from books 
+	where genre ='Fiction';
+
+--2) Find books published after the year 1950
+
+select * from books
+	where published_year > 1950;
+
+--3) List all customers from the Canada
+select * from customers
+	where country ='Canada';
+
+--4) Show orders placed in November 2023
+select * from orders 
+	where order_date between '2023-11-01' and '2023-11-30';
+	
+--5) Retrieve the total stock of books available
+
+select sum(stock) As total_stock
+from books;
+
+--6) Find the details of the most expensive book
+select * from books
+order by price desc
+limit 1;
+
+--7) Show all customers who ordered more than 1 quantity of a book
+
+select * from orders 
+where quantity>1 ;
+
+--8) Retrieve all orders where the total amount exceeds $20
+select * from orders 
+where total_amount > 20;
+
+--9) List all genres available in the Books table
+select distinct genre from books;
+
+--10) Find the book with the lowest stock
+select * from books
+order by stock asc
+limit 1;
+--11) Calculate the total revenue generated from all orders
+select sum(total_amount) as Total_revenue
+from orders;
+
+
+--12) Retrieve the total number of books sold for each genre
+select b.genre, sum(quantity) as total_sold
+from books as b
+join orders as o
+on b.book_id=o.book_id
+group by b.genre;
+
+
+
+--13) Find the average price of books in the "Fantasy" genre
+select avg(price) as  avg_price
+from books 
+where genre='Fantasy';
+
+--14) List customers who have placed at least 2 orders
+
+select c.name,o.customer_id, count(order_id) as order_count
+from customers as c 
+join orders as o 
+on c.customer_id = o.customer_id
+group by c.name,o.customer_id
+having count(order_id)>=2;
+
+--15)Find the most frequently ordered book
+select b.title,b.genre,o.book_id, count(order_id) as Times_of_ordered
+from books as b
+join orders as o 
+on b.book_id= o.book_id
+group by b.title,b.genre,o.book_id
+order by count(order_id) desc
+limit 1;
+
+--16) Show the top 3 most expensive books of 'Fantasy' Genre 
+select book_id,title,genre ,price
+from books
+where genre='Fantasy'
+order by price desc
+limit 3 offset 0;
+
+--17)Retrieve the total quantity of books sold by each author
+select  b.author, sum(o.quantity) as Total_sold_by_author
+from books as b
+join orders as o 
+on b.book_id= o.book_id
+group by  b.author;
+
+--18) List the cities where customers who spent over 30 are located
+select distinct c.city,total_amount
+from customers as c 
+join orders as o
+on c.customer_id=o.customer_id
+where total_amount >30;
+
+--19)Find the customer who spent the most on orders
+
+select c.name,sum(o.total_amount) as top_customer
+from customers as c
+join orders as o
+on c.customer_id=o.customer_id 
+group by c.name
+order by sum(o.total_amount) desc
+limit 1;
+
+
+--20)Calculate the stock remaining after fulfilling all orders
+select b.book_id, b.title, b.stock, coalesce(sum(quantity),0)as order_quantity,
+b.stock- coalesce (sum(quantity),0) as remaining_stock
+from books as b
+left join orders as o 
+on b.book_id=o.book_id
+group by b.book_id, b.title, b.stock;
+
+
+
